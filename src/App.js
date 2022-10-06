92dd6f1ac68f79c079540923264d4bda19e5b9ac
i476933@fhict.nl <476933@student.fontys.nl>
2022-10-06 09:17:25 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Yenlo Planner</h1>
+        <h1>Yenlo Planner Test Push</h1>
       </header>
     </div>
   );
