bc5a712b606e848e030e93990a397e0644f27a29
Wim Pluijm <437045@student.fontys.nl>
2022-11-24 13:53:26 +0100

    Kleine change css Schedule

--- src/components/layout/planner/Schedule.jsx
+++ src/components/layout/planner/Schedule.jsx
@@ -1,243 +1,312 @@
 import React, { useState } from "react";
-import './Schedule.css';
+import "./Schedule.css";
 import { useNavigate } from "react-router-dom";
 import { fontStyle, fontWeight } from "@mui/system";
 import { useEffect } from "react";
 
 function Schedule() {
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const [monthName, setMonthName] = useState(nameOfMonth(selectedDate));
-    const navigate = useNavigate();
-    useEffect(() => {
-        },[monthName]);
-    return [
-        <div className="scheduleMain">
-            <h1 className="ScheduleHeader"><a onClick={() => moveMonths(-1)}>←</a> {monthName} <a onClick={() => moveMonths(1)}>→</a> </h1>
-            <div class="daysTable">
-                
-                <div class="dayNames">
-                </div>
-                <p>Mo</p>
-                <p>Tu</p>
-                <p>We</p>
-                <p>Th</p>
-                <p>Fr</p>
-                {allDays()}
-            </div>
-        </div>,
-        <button className="attendanceBtn" onClick={() => navigate("./edit", { replace: true })}>Edit attendance</button>,
-        Legend()]
-
-function Legend() {
-  return (
-    <div className="LegendMain">
-      <h1 className="LegendHeader">Legend</h1>
-      <div className="LegendBody">
-        <ul>
-          <li key="1">
-            <div className="Office">
-              <p>Office</p>
-            </div>
-          </li>
-          <li key="2">
-            <div className="Home">
-              <p>Home</p>
-            </div>
-          </li>
-          <li key="3">
-            <div className="Customer">
-              <p>Customer</p>
-            </div>
-          </li>
-          <li key="4">
-            <div className="Sick">
-              <p>Sick</p>
-            </div>
-          </li>
-          <li key="5">
-            <div className="Leave">
-              <p>Leave</p>
-            </div>
-          </li>
-        </ul>
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [monthName, setMonthName] = useState(nameOfMonth(selectedDate));
+  const navigate = useNavigate();
+  useEffect(() => {}, [monthName]);
+  return [
+    <div className="scheduleMain">
+      <h1 className="ScheduleHeader">
+        <a onClick={() => moveMonths(-1)}>←</a> {monthName}{" "}
+        <a onClick={() => moveMonths(1)}>→</a>{" "}
+      </h1>
+      <div class="daysTable">
+        <div class="dayNames"></div>
+        <p>Mo</p>
+        <p>Tu</p>
+        <p>We</p>
+        <p>Th</p>
+        <p>Fr</p>
+        {allDays()}
       </div>
-    </div>
-  );
-}
-
-function allDays() {
-  const availabilityPairs = getUserData();
-  let days = [];
-  let addDays = 0;
-  for (let index = 0; index < 25; index++) {
-    days[index] = Day(
-      index,
-      0,
-      0,
-      new Date(firstDate.getTime() + 86400000 * addDays)
-    );
-    if ((index + 1) % 5 === 0) {
-      addDays += 3;
-    } else if ((index + 1) % 6 !== 0 || (index + 1) % 7 !== 0) addDays++;
-  }
-
-  for (let i = 0; i < availabilityPairs.length; i++) {
-    const avaPair = availabilityPairs[i];
-    let n = findIndexByDate(avaPair[0].dateTime);
-    addDays = 0;
-    for (let j = 0; j < 25; j++) {
-      if (j === n) {
-        if (avaPair[1] != null) {
-          days[j] = Day(
-            j,
-            avaPair[0].status,
-            avaPair[1].status,
-            new Date(firstDate.getTime() + 86400000 * addDays)
-          );
-        } else {
-          const ava = avaPair[0];
-          if (ava.beforeMidday)
-            days[j] = Day(
-              j,
-              avaPair[0].status,
-              0,
-              new Date(firstDate.getTime() + 86400000 * addDays)
-            );
-          else
-            days[j] = Day(
-              j,
-              0,
-              avaPair[0].status,
-              new Date(firstDate.getTime() + 86400000 * addDays)
-            );
-        }
-        break;
-      }
-
-      if ((j + 1) % 5 === 0) {
-        addDays += 3;
-      } else if ((j + 1) % 6 !== 0 || (j + 1) % 7 !== 0) addDays++;
-    }
-  }
-  return days;
-}
-
-function Day(index, morning, noon, date) {
-  return (
-    <div className="Index" id={date.toISOString().split("T")[0]}>
-      <p className="dateNum">{date.getDate()}</p>
-      <div
-        id={`morning-${index}`}
-        style={{ backgroundColor: `var(--${ColorByStatus(morning)})` }}
-        className="Top"
-      >
-        <span class="plannerTooltip">0 people in office</span>
-      </div>
-      <div
-        id={`midday-${index}`}
-        style={{ backgroundColor: `var(--${ColorByStatus(noon)})` }}
-        className="Bottom"
-      >
-        <span class="plannerTooltip">0 people in office</span>
-      </div>
-    </div>
-  );
-}
-
-function ColorByStatus(status) {
-  switch (status) {
-    case 0:
-      return "gray";
-    case 1:
-      return "clrOffice";
-    case 2:
-      return "clrHome";
-    case 3:
-      return "clrCustomer";
-    case 4:
-      return "clrSick";
-    case 5:
-      return "clrLeave";
-    default:
-      return "gray";
-  }
-}
-
-function getUserData() {
-  // retrieve data here:
-  let availabilities = [
-    {
-      beforeMidday: true,
-      dateTime: new Date("2022-11-03"),
-      status: 3,
-    },
-    {
-      beforeMidday: false,
-      dateTime: new Date("2022-11-03"),
-      status: 2,
-    },
-    {
-      beforeMidday: false,
-      dateTime: new Date("2022-11-08"),
-      status: 4,
-    },
-    {
-      beforeMidday: false,
-      dateTime: new Date("2022-10-31"),
-      status: 5,
-    },
+    </div>,
+    <button
+      className="attendanceBtn"
+      onClick={() => navigate("./edit", { replace: true })}
+    >
+      Edit attendance
+    </button>,
+    Legend(),
   ];
 
-  // sort array
-  let result = [];
-  for (let i = 0; i < availabilities.length; i++) {
-    const ava = availabilities[i];
-    let newIndex = true;
-    for (let j = 0; j < result.length; j++) {
-      const resultAva = result[j];
-      if (isSameDay(resultAva[0].dateTime, ava.dateTime)) {
-        result[j] = [resultAva[0], ava];
-        newIndex = false;
-        break;
+  function Legend() {
+    return (
+      <div className="LegendMain">
+        <h1 className="LegendHeader">Legend</h1>
+        <div className="LegendBody">
+          <ul>
+            <li key="1">
+              <div className="Office">
+                <p>Office</p>
+              </div>
+            </li>
+            <li key="2">
+              <div className="Home">
+                <p>Home</p>
+              </div>
+            </li>
+            <li key="3">
+              <div className="Customer">
+                <p>Customer</p>
+              </div>
+            </li>
+            <li key="4">
+              <div className="Sick">
+                <p>Sick</p>
+              </div>
+            </li>
+            <li key="5">
+              <div className="Leave">
+                <p>Leave</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  function allDays() {
+    var firstDate = firstIndex(selectedDate);
+    const availabilityPairs = getUserData();
+    let days = [];
+    let addDays = 0;
+    for (let index = 0; index < 25; index++) {
+      days[index] = Day(
+        index,
+        0,
+        0,
+        new Date(firstDate.getTime() + 86400000 * addDays)
+      );
+      if ((index + 1) % 5 === 0) {
+        addDays += 3;
+      } else if ((index + 1) % 6 != 0 || (index + 1) % 7 != 0) addDays++;
+    }
+
+    for (let i = 0; i < availabilityPairs.length; i++) {
+      const avaPair = availabilityPairs[i];
+      let n = findIndexByDate(avaPair[0].dateTime, days);
+      addDays = 0;
+      for (let j = 0; j < 25; j++) {
+        if (j === n) {
+          if (avaPair[1] != null) {
+            days[j] = Day(
+              j,
+              avaPair[0].status,
+              avaPair[1].status,
+              new Date(firstDate.getTime() + 86400000 * addDays)
+            );
+          } else {
+            const ava = avaPair[0];
+            if (ava.beforeMidday)
+              days[j] = Day(
+                j,
+                avaPair[0].status,
+                0,
+                new Date(firstDate.getTime() + 86400000 * addDays)
+              );
+            else
+              days[j] = Day(
+                j,
+                0,
+                avaPair[0].status,
+                new Date(firstDate.getTime() + 86400000 * addDays)
+              );
+          }
+          break;
+        }
+
+        if ((j + 1) % 5 === 0) {
+          addDays += 3;
+        } else if ((j + 1) % 6 != 0 || (j + 1) % 7 != 0) addDays++;
       }
     }
-    if (newIndex) result.push([ava]);
+    return days;
   }
-  return result;
-}
 
-function findIndexByDate(date) {
-  const index = document.getElementById(
-    new Date(date).toISOString().split("T")[0]
-  );
-  if (index != null) {
-    for (const child of index.childNodes) {
-      if (child.id.startsWith("morning-")) {
-        return Number(child.id.split("-")[1]);
-      }
+  function Day(index, morning, noon, date) {
+    var topClass = "Top";
+    var bottomClass = "Bottom";
+    var textStyle = {};
+    if (isSameDay(date, Date.now())) {
+      topClass = "TopToday";
+      bottomClass = "BottomToday";
+      textStyle = { fontWeight: "bold" };
+    }
+    return (
+      <div className="Index" id={date.toISOString().split("T")[0]}>
+        <p className="dateNum" style={textStyle}>
+          {date.getDate()}
+        </p>
+        <div
+          id={`morning-${index}`}
+          style={{ backgroundColor: `var(--${ColorByStatus(morning)})` }}
+          className={topClass}
+        >
+          <span class="plannerTooltip">0 people in office</span>
+        </div>
+        <div
+          id={`midday-${index}`}
+          style={{ backgroundColor: `var(--${ColorByStatus(noon)})` }}
+          className={bottomClass}
+        >
+          <span class="plannerTooltip">0 people in office</span>
+        </div>
+      </div>
+    );
+  }
+
+  function ColorByStatus(status) {
+    switch (status) {
+      case 0:
+        return "gray";
+      case 1:
+        return "clrOffice";
+      case 2:
+        return "clrHome";
+      case 3:
+        return "clrCustomer";
+      case 4:
+        return "clrSick";
+      case 5:
+        return "clrLeave";
+      default:
+        return "gray";
     }
   }
-  return -1;
-  // TODO: have this return the index in the planner based on the given date.
-}
 
-function isSameDay(date1, date2) {
-  date1 = new Date(date1);
-  date2 = new Date(date2);
-  return (
-    date1.getFullYear() === date2.getFullYear() &&
-    date1.getMonth() === date2.getMonth() &&
-    date1.getDate() === date2.getDate()
-  );
-}
+  function moveMonths(amount) {
+    let newMonth = selectedDate.getMonth() + amount;
+    let yearsToAdd = 0;
+    if (amount > 0) {
+      for (let i = 0; newMonth > 12; i++) {
+        newMonth = newMonth - 12;
+        yearsToAdd++;
+      }
+    } else if (amount < 0) {
+      for (let i = 0; newMonth < 1; i++) {
+        newMonth = newMonth + 12;
+        yearsToAdd--;
+      }
+    }
+    var d = selectedDate;
+    d.setMonth(newMonth);
+    d.setFullYear(selectedDate.getFullYear() + yearsToAdd);
+    setSelectedDate(d);
+    setMonthName(nameOfMonth(d));
+  }
 
-function firstIndex(date) {
-  const now = new Date(date);
-  const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
-  const day = firstDay.getDay();
-  let diff = firstDay.getDate() - day + (day === 0 ? -6 : 1); // adjust when day is sunday
-  return new Date(now.setDate(diff));
-}
+  function nameOfMonth(date) {
+    switch (new Date(date).getMonth()) {
+      case 0:
+        return "January";
+      case 1:
+        return "February";
+      case 2:
+        return "March";
+      case 3:
+        return "April";
+      case 4:
+        return "May";
+      case 5:
+        return "June";
+      case 6:
+        return "July";
+      case 7:
+        return "August";
+      case 8:
+        return "September";
+      case 9:
+        return "October";
+      case 10:
+        return "November";
+      case 11:
+        return "December";
+      default:
+        return "Unkown month";
+    }
+  }
 
+  function getUserData() {
+    // retrieve data here:
+    let availabilities = [
+      {
+        beforeMidday: true,
+        dateTime: new Date("2022-11-03"),
+        status: 3,
+      },
+      {
+        beforeMidday: false,
+        dateTime: new Date("2022-11-03"),
+        status: 2,
+      },
+      {
+        beforeMidday: false,
+        dateTime: new Date("2022-11-08"),
+        status: 4,
+      },
+      {
+        beforeMidday: false,
+        dateTime: new Date("2022-10-31"),
+        status: 5,
+      },
+      {
+        beforeMidday: true,
+        dateTime: new Date("2022-12-12"),
+        status: 5,
+      },
+    ];
+
+    // sort array
+    let result = [];
+    for (let i = 0; i < availabilities.length; i++) {
+      const ava = availabilities[i];
+      let newIndex = true;
+      for (let j = 0; j < result.length; j++) {
+        const resultAva = result[j];
+        if (isSameDay(resultAva[0].dateTime, ava.dateTime)) {
+          result[j] = [resultAva[0], ava];
+          newIndex = false;
+          break;
+        }
+      }
+      if (newIndex) result.push([ava]);
+    }
+    return result;
+  }
+
+  function findIndexByDate(date, allDays) {
+    let res = -1;
+    allDays.forEach((d) => {
+      if (d.props.id === new Date(date).toISOString().split("T")[0]) {
+        res = Number(d.props.children[1].props.id.split("-")[1]);
+      }
+    });
+    return res;
+  }
+
+  function isSameDay(date1, date2) {
+    date1 = new Date(date1);
+    date2 = new Date(date2);
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  }
+
+  function firstIndex(date) {
+    const now = new Date(date);
+    const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
+    const day = firstDay.getDay();
+    let diff = firstDay.getDate() - day + (day == 0 ? -6 : 1);
+    return new Date(now.setDate(diff));
+  }
+}
 
 export default Schedule;
