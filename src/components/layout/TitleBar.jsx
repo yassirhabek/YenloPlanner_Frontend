4519d2e995ee2a868648297cc55379bf5eb36b97
Yassir Habek <485050@student.fontys.nl>
2022-11-28 10:18:53 +0100

    searchbar changes

--- src/components/layout/TitleBar.jsx
+++ src/components/layout/TitleBar.jsx
@@ -4,23 +4,62 @@ import classes from "./TitleBar.module.css";
 import SearchBar from "./SearchBar";
 import YenloLogo from "../../assets/YenloLogo.PNG";
 import Uitloggen from "../../assets/Uitloggen.PNG";
+import { useEffect, useState } from "react";
 
 function TitleBar() {
+  const [werknemers, setWerknemers] = useState([]);
+  const [teams, setTeams] = useState([]);
+
   const location = useLocation();
   let Titlebar;
+
+  useEffect(() => {
+    getAllWerknemers();
+    getAllTeams();
+  }, [window.location.pathname]);
 
   function logOutHandler() {
     alert("Log out method functionality.");
   }
 
-  const Data = [
-    { name: "Matthijs", id: 1 },
-    { name: "Justin", id: 2 },
-    { name: "Wim", id: 3 },
-    { name: "Pascal", id: 4 },
-    { name: "Boris", id: 5 },
-    { name: "Yassir", id: 6 },
-  ];
+  async function getAllWerknemers() {
+    try {
+      const response = await fetch("http://localhost:8080/user", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        accept: "application/json",
+      });
+      var result = await response.json();
+
+      console.log(result);
+      setWerknemers(result);
+      return result;
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+  
+  async function getAllTeams() {
+    try {
+      const response = await fetch("http://localhost:8080/team", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        accept: "application/json",
+      });
+      var result = await response.json();
+
+      console.log(result);
+      setTeams(result);
+      return result;
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
   const TeamData = [
     { name: "Project Fontys", id: 1 },
     { name: "Project Planner", id: 2 },
@@ -31,7 +70,7 @@ function TitleBar() {
     Titlebar = (
       <header className={classes.header}>
         <img src={YenloLogo} alt="Yenlo Logo" className={classes.logo}/>
-        <SearchBar placeholder="Search Employee..." data={Data} className={classes.search}/>
+        <SearchBar placeholder="Search Employee..." data={werknemers} className={classes.search}/>
         <img
           src={Uitloggen}
           alt="Uitloggen"
@@ -44,7 +83,7 @@ function TitleBar() {
     Titlebar = (
       <header className={classes.header}>
         <img src={YenloLogo} alt="Yenlo Logo" className={classes.logo}/>
-        <SearchBar placeholder="Search Employee..." data={Data} className={classes.search}/>
+        <SearchBar placeholder="Search Employee..." data={werknemers} className={classes.search}/>
         <img
           src={Uitloggen}
           alt="Uitloggen"
@@ -58,7 +97,7 @@ function TitleBar() {
     Titlebar = (
       <header className={classes.header}>
         <img src={YenloLogo} alt="Yenlo Logo" className={classes.logo}/>
-        <SearchBar placeholder="Search Team..." data={TeamData} className={classes.search} />
+        <SearchBar placeholder="Search Team..." data={teams} className={classes.search} />
         <img
           src={Uitloggen}
           alt="Uitloggen"
@@ -71,7 +110,7 @@ function TitleBar() {
     Titlebar = (
       <header className={classes.header}>
         <img src={YenloLogo} alt="Yenlo Logo" className={classes.logo}/>
-        <SearchBar placeholder="Search Team..." data={TeamData} className={classes.search} />
+        <SearchBar placeholder="Search Team..." data={teams} className={classes.search} />
         <img
           src={Uitloggen}
           alt="Uitloggen"
