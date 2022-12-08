f44d341f611c3916acecadb4488ca80e438f3d70
Yassir Habek <485050@student.fontys.nl>
2022-12-08 14:04:06 +0100

    user context WARNING: werkt nog niet

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
+import { useContext } from 'react';
+
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import './Login.css';
+import UserContext from '../../store/logged-in-context.js';
 import YenloLogo from '../../assets/YenloLogo.PNG';
 
 function Copyright(props) {
@@ -33,33 +31,15 @@ const theme = createTheme();
 
 function Login() {
 
+  const userCtx = useContext(UserContext);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     var username = data.get('username');
     var password = data.get('password');
     console.log(username, password);
-
-    const response = await fetch('http://localhost:8080/auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
-    });
-
-    const result = await response.json();
-
-    if (response.ok) {
-
-      document.cookie = 'token=' + result.accessToken;
-      window.location.href = '/user-planner';
-    } else {
-      alert('Login failed');
-    }
+    userCtx.onLogin(username, password);
   };
 
   return (
@@ -74,7 +54,7 @@ function Login() {
             alignItems: 'center',
           }}
         >
-          <img src={YenloLogo} class="logo" />
+          <img src={YenloLogo} class="logo" alt="Yenlo Logo" />
 
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
